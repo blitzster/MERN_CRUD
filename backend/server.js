d44fe9f45649bbb2e345c3e8708f86b36dbeb9af
Akshay S <blitz122333@gmail.com>
2024-11-10 22:40:35 +0530

    API done

--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,19 @@
-// const express = require('express');
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
-import Product from './models/product.model.js';
+import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
 const app = express();
 
-app.post("/products", async (req, res) => {
-    const product = req.body; //user will send this data.
+// Middleware to parse JSON data
+app.use(express.json());
 
-    if (!product.name || !product.price || !product.image) {
-        return res.status(400).json({ success: false, message: "Please provide all fields." });
-    }
-
-    const newProduct = new Product(product);
-
-    try {
-        await newProduct.save();
-        res.status(201).json({ success: true, data: newProduct });
-    } catch (error) {
-        console.error("Error in Create Product:", error.message);
-        res.status(500).json({ success: false, message: "Server Error" });
-    }
-});
+app.use("/api/products", productRoutes);
 
 
 app.listen(5000, () => {
     connectDB();
     console.log('Server started at http://localhost:5000');
-})
-
-
-
-
+});
